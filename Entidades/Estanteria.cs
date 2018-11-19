2c6c59529cf0e850cc1ca6bbb1596501e726ef67
Xabier Claver Zabala <xabierclaver99@gmail.com>
2018-11-19 13:23:25 +0100

    Estanteria arreglado y añadido estante

--- Entidades/Estanteria.cs
+++ Entidades/Estanteria.cs
@@ -8,20 +8,22 @@ namespace Entidades
 {
     class Estanteria : IEquatable<Estanteria>
     {
+        public int Idestateria { get; set; }
+        public int NumAlturas { get; set; }
+        public int NumFilas { get; set; }
+        public List<Estante> listaEstantes { get; set; }
+
         public Estanteria()
         {
         }
 
-        public Estanteria(int idestateria, int numAlturas, int numFilas)
+        public Estanteria(int idestateria, int numAlturas, int numFilas, List<Estante> listaEstantes)
         {
             Idestateria = idestateria;
             NumAlturas = numAlturas;
             NumFilas = numFilas;
+            this.listaEstantes = listaEstantes;
         }
-
-        public int Idestateria { get; set; }
-        public int NumAlturas { get; set; }
-        public int NumFilas { get; set; }
 
         public override bool Equals(object obj)
         {
@@ -31,18 +33,12 @@ namespace Entidades
         public bool Equals(Estanteria other)
         {
             return other != null &&
-                   Idestateria == other.Idestateria &&
-                   NumAlturas == other.NumAlturas &&
-                   NumFilas == other.NumFilas;
+                   Idestateria == other.Idestateria;
         }
 
         public override int GetHashCode()
         {
-            var hashCode = -393419837;
-            hashCode = hashCode * -1521134295 + Idestateria.GetHashCode();
-            hashCode = hashCode * -1521134295 + NumAlturas.GetHashCode();
-            hashCode = hashCode * -1521134295 + NumFilas.GetHashCode();
-            return hashCode;
+            return -1726190668 + Idestateria.GetHashCode();
         }
     }
 }
