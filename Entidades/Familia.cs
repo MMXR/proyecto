ee3ea942d95fd4cd72a505d944676cbf01278758
Xabier Claver Zabala <xabierclaver99@gmail.com>
2018-11-19 13:13:22 +0100

    Arreglos y añadir subFamilia

--- Entidades/Familia.cs
+++ Entidades/Familia.cs
@@ -11,12 +11,14 @@ namespace Entidades
         public int codFamilia { get; set; }
         public String idString { get; set; }
         public String descripcion { get; set; }
+        public List<SubFamilia> listaSubFamilias { get; set; }
 
-        public Familia(int codFamilia, string idString, string descripcion)
+        public Familia(int codFamilia, string idString, string descripcion, List<SubFamilia> listaSubFamilias)
         {
             this.codFamilia = codFamilia;
             this.idString = idString;
             this.descripcion = descripcion;
+            this.listaSubFamilias = listaSubFamilias;
         }
 
         public Familia()
---
+++ Entidades/SubFamilia.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class SubFamilia : IEquatable<SubFamilia>
+    {
+        public int codSF { get; set; }
+        public String idCaracter { get; set; }
+        public String descripcion { get; set; }
+        public int codFamilia { get; set; }
+
+
+        public SubFamilia(int codSF, string idCaracter, string descripcion, int codFamilia)
+        {
+            this.codSF = codSF;
+            this.idCaracter = idCaracter;
+            this.descripcion = descripcion;
+            this.codFamilia = codFamilia;
+        }
+
+        public SubFamilia()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as SubFamilia);
+        }
+
+        public bool Equals(SubFamilia other)
+        {
+            return other != null &&
+                   codSF == other.codSF &&
+                   idCaracter == other.idCaracter &&
+                   descripcion == other.descripcion &&
+                   codFamilia == other.codFamilia;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -104265189;
+            hashCode = hashCode * -1521134295 + codSF.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(idCaracter);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(descripcion);
+            hashCode = hashCode * -1521134295 + codFamilia.GetHashCode();
+            return hashCode;
+        }
+    }
+}
