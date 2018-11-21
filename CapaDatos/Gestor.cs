9de153d8bcd42556ad9f16c866ef24619fcbe907
Miguel Sada <msada@hotmail.es>
2018-11-21 11:46:36 +0100

    Cambio de nombre a class1-->Gestor

---
+++ CapaDatos/Gestor.cs
@@ -0,0 +1,49 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Gestor
+    {
+
+        //CRUD Producto
+        public void createProduct(Producto p)
+        {
+
+        }
+
+        //Read-->Opciones varias
+        public List<Producto> selectProductNombre(String name)
+        {
+            List<Producto> productos = new List<Producto>();
+            return productos;
+        }
+
+        public void selectProductCode(String code)
+        {
+
+        }
+
+        public void selectAllProducts()
+        {
+
+        }
+
+        //Update
+        public void updateProduct(Producto p)
+        {
+
+        }
+
+        //Delete
+        public void deleteProduct(Producto p)
+        {
+
+        }
+
+    }
+}
